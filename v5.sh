d1f784a3d9e110ae51c68c6ac996be4bd6fd0322
MrYou12 <74676768+MrYou12@users.noreply.github.com>
2020-11-20 17:15:29 +0700

    v5

---
+++ v5.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+#Perbuatan Anda Dosa Anda!
+clear
+echo "________________________________"
+echo "╔╦╦╗╔═╗╔╗─╔═╗╔═╗╔══╗╔═╗"
+echo "║║║║║╩╣║╚╗║═╣║╬║║║║║║╩╣"
+echo "╚══╝╚═╝╚═╝╚═╝╚═╝╚╩╩╝╚═╝"
+echo "________________________________"
+echo "Authour : MrYou12"
+echo "Kontak : 089514741440"
+echo "YouTube : Andika You12"
+echo "Web : https://github.com/MrYou12"
+echo "________________________________"
+echo
+echo "[ ☣P I L I H M E N U☣ ]"
+echo "======================="
+echo "[1] Hack Fb [On]"
+echo "[2] BruteForce [On]"
+echo "[3] MoontonChecker [On]"
+echo "[4] FacebookChecker [On]"
+echo "[5] HackIG [On]"
+echo "[6] Lihat Map [On]"
+echo "[7] Animasi Api [On]"
+echo "[11] Exit"
+read -p" [ H A C K E D ]" pill
+#Batas Gan
+if [ $pill = "1" ]
+then
+echo "╔═╗╔═╗╔═╗╔══╗╔═╗╔══╗"
+echo "║╬║║╬║║║║║══╣║╦╝║══╣"
+echo "║╔╝║╗╣║║║╠══║║╩╗╠══║"
+echo "╚╝─╚╩╝╚═╝╚══╝╚═╝╚══╝" ;sleep2
+pkg update && pkg upgrade
+pkg install curl git python2
+pkg install git
+pkg install curl
+pip2 install mechanize
+pip2 install requests
+git clone https://github.com/MasUzie/Hackfb.git
+cd Hackfb
+python2 hackfb.py
+echo "S E L E S A I!"
+fi
+#Batas Gan
+if [ $pill = "2" ]
+then
+echo "╔═╗╔═╗╔═╗╔══╗╔═╗╔══╗"
+echo "║╬║║╬║║║║║══╣║╦╝║══╣"
+echo "║╔╝║╗╣║║║╠══║║╩╗╠══║"
+echo "╚╝─╚╩╝╚═╝╚══╝╚═╝╚══╝" ;sleep2
+pkg install git
+pkg install python2
+pip2 install requests
+pip2 install mechanize
+pip2 install bs4
+git clone https://github.com/FR13ND8/BruteFb
+cd BruteFb
+python2 brute.py
+echo "S E L E S A I!"
+fi
+#Batas Gan
+if [ $pill = "3" ]
+then
+echo "╔═╗╔═╗╔═╗╔══╗╔═╗╔══╗"
+echo "║╬║║╬║║║║║══╣║╦╝║══╣"
+echo "║╔╝║╗╣║║║╠══║║╩╗╠══║"
+echo "╚╝─╚╩╝╚═╝╚══╝╚═╝╚══╝" ;sleep2
+pkg update && pkg upgrade
+pkg install git
+pkg install python
+pkg install python2
+pkg install php
+pip2 install requests
+git clone https://github.com/dz-id/MoontonChecker
+cd MoontonChecker
+python moonton.py
+echo "S E L E S A I!"
+fi
+#Batas Gan
+if [ $pill = "4" ]
+then
+echo "╔═╗╔═╗╔═╗╔══╗╔═╗╔══╗"
+echo "║╬║║╬║║║║║══╣║╦╝║══╣"
+echo "║╔╝║╗╣║║║╠══║║╩╗╠══║"
+echo "╚╝─╚╩╝╚═╝╚══╝╚═╝╚══╝" ;sleep2
+pkg install git
+pkg install php
+pkg install bash
+git clone https://github.com/f0xbase/Facebook-Checker
+cd Facebook-Checker
+php fbchecker.php list.txt
+echo "S E L E S A I!"
+fi
+#Batas Gan
+if [ $pill = "5" ]
+then
+echo "╔═╗╔═╗╔═╗╔══╗╔═╗╔══╗"
+echo "║╬║║╬║║║║║══╣║╦╝║══╣"
+echo "║╔╝║╗╣║║║╠══║║╩╗╠══║"
+echo "╚╝─╚╩╝╚═╝╚══╝╚═╝╚══╝" ;sleep2
+pkg install git
+pkg install python2
+pkg install jq -y
+git clone https://github.com/noolep/multiBF_ig
+cd multiBF_ig
+bash brute.sh
+echo "S E L E S A I!"
+fi
+#Batas Gan
+if [ $pill = "6" ]
+then
+echo "╔═╗╔═╗╔═╗╔══╗╔═╗╔══╗"
+echo "║╬║║╬║║║║║══╣║╦╝║══╣"
+echo "║╔╝║╗╣║║║╠══║║╩╗╠══║"
+echo "╚╝─╚╩╝╚═╝╚══╝╚═╝╚══╝" ;sleep2
+pkg install git
+pkg install perl
+git clone https://github.com/x-xsystm/maps.git
+cd maps
+perl maps.pl
+echo "S E L E S A I!"
+echo "---------------"
+echo "Pass:pejuang212"
+echo "---------------"
+fi
+#Batas Gan
+if [ $pill = "7" ]
+then
+echo "╔═╗╔═╗╔═╗╔══╗╔═╗╔══╗"
+echo "║╬║║╬║║║║║══╣║╦╝║══╣"
+echo "║╔╝║╗╣║║║╠══║║╩╗╠══║"
+echo "╚╝─╚╩╝╚═╝╚══╝╚═╝╚══╝" ;sleep2
+pkg install libcaca
+cacafire
+echo "S E L E S A I!"
+fi
+#Batas Gan
